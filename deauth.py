be78791f9cb734c7b69313bbb2bf0580c33d5b40
Aayush <aayush.077bct06@acem.edu.np>
2024-07-13 17:50:11 +0545

    project related paper added and scan network code changed

--- deauth.py
+++ deauth.py
@@ -1,12 +1,31 @@
-import pywifi
+from scapy.all import *
+import json
 
-def scan_wifi():
-    wifi=pywifi.PyWiFi()
-    iface=wifi.interfaces()[0]
-    iface.scan()
-    scan_results = iface.scan_results()
-    for network in scan_results:
-        print(f"SSID:{network.ssid},BSSID:{network.bssid}")
+networks=[]
 
-scan_wifi()
+def save_network():
+    with open("wifi_networks.josn","w") as f:
+        f.write(json.dumps(networks))
 
+
+def scan_wifi(packet):
+    if packet.haslayer(Dot11Beacon):
+        bssid=packet[Dot11].addr2
+        ssid=packet[Dot11Elt].info.decode()
+        stats=packet[Dot11Beacon].network_stats()
+        channel=stats.get("channel")
+        crypto=stats.get("crypto")
+
+        if "WPA/PSK" in crypto or "WPA2/PSK" in crypto:
+            data={
+                "ssid":ssid,
+                "bssid":bssid,
+                "channel":channel,
+                "crypto":crypto
+            }
+            networks.append(data)
+
+
+if __name__ == "__main__":
+    sniff(prn=scan_wifi,iface="wlan0mon",timeout=5)
+    save_network()
